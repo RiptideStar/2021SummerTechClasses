f2cbc91fc7093009122895897c813a1988f2d666
Riptide_mac <kyle1001001@gmail.com>
2021-07-21 12:17:13 -0700

    Week 3 (OpenCV Problems)

--- AI_in_Python/FaceDetection/Haar/face_detector.py
+++ AI_in_Python/FaceDetection/Haar/face_detector.py
@@ -7,15 +7,17 @@ img = cv2.imread('federer.jpg')
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 face_coordinates = trained_face_data.detectMultiScale(img)
-print(face_coordinates)
+print(face_coordinates) # list of coordinates (x, y, w, h)
 
 
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0,0,255), 2)
 
-# #Display
-# windowName = 'FaceDetector'
-# cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
-# cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-# cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
-# cv2.imshow(windowName, img)
-# cv2.waitKey() # waits for a key stroke before exiting
 
+#Display
+windowName = 'FaceDetector'
+cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+cv2.imshow(windowName, img)
+cv2.waitKey() # waits for a key stroke before exiting
