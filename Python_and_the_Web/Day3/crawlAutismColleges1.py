4c82db0a72cc7640adf66bede002c8229d417674
Riptide_mac <kyle1001001@gmail.com>
2021-07-30 15:02:29 -0400

    Week 4

--- Python_and_the_Web/Day3/crawlAutismColleges1.py
+++ Python_and_the_Web/Day3/crawlAutismColleges1.py
@@ -1,12 +1,15 @@
 import requests
 import sys
 from bs4 import BeautifulSoup #html parser
+import xlwt
+
 
 print("--- Command Line:", sys.argv)
 
 api_url = "https://www.bestvalueschools.com/rankings/students-with-autism/"
 print("--- api_url:", api_url)
 
+# takes in url, outputs a universities list of relevant information
 def retrieveData(api_url):
     try:
         response = requests.get(api_url)
@@ -23,8 +26,7 @@ def retrieveData(api_url):
     # print(elements)
     
     datalist = []
-    sum = 0
-    # university name, location, ranking, description, url of college, api_url
+    # row content: university name, location, ranking, description, url of college, api_url
     for i in range(0, len(elements)):
         row = []
         univ_name = elements[i].find('span').getText()
@@ -41,6 +43,35 @@ def retrieveData(api_url):
         row.append(desc)
         url = elements[i].findChild('a')['href']
         # print(i, url)
+        row.append(url)
+        row.append(api_url)
+        datalist.append(row)
 
+    return datalist
 
-retrieveData(api_url)
+datalist = retrieveData(api_url)
+# print(datalist)
+
+def save_to_excel(datalist):
+    # create book
+    book = xlwt.Workbook(encoding='utf-8', style_compression=0)
+
+    # create sheet
+    sheet = book.add_sheet('universitiesForAutismStudents', cell_overwrite_ok=True)
+
+    # column setup
+    columns = {'Univ Name', 'Location', 'Ranking', 'Description', 'URL', 'Source', 'Misc'}
+
+    #write our columns into the top row of the excel sheet
+    for i in range(0, len(columns)):
+        sheet.write(0, i, columns[i])
+
+    # write our data in for every row past the top column row
+    for i in range(0, len(datalist)):
+        data = datalist[i]
+        for j in range(0, len(data)):
+            sheet.write(i+1, j, data[j])
+            # print(i, data[j])
+
+# save_to_excel(datalist)
+
