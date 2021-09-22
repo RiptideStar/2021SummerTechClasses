a2a10709637c347e11d618bea470dfd1a7f9dbcd
Riptide_mac <kyle1001001@gmail.com>
2021-09-21 23:17:21 -0700

    using the rock as our image

--- AI_in_Python/FaceDetection/Haar/face_detector.py
+++ AI_in_Python/FaceDetection/Haar/face_detector.py
@@ -8,39 +8,39 @@ cv2.setWindowProperty(windowName, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-webcam = cv2.VideoCapture(0)
-while True:
-    readFrame, frame = webcam.read()
+# webcam = cv2.VideoCapture(0)
+# while True:
+#     readFrame, frame = webcam.read()
 
-    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+#     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-    for (x, y, w, h) in face_coordinates:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,0,255), 2)
+#     for (x, y, w, h) in face_coordinates:
+#         cv2.rectangle(frame, (x, y), (x+w, y+h), (0,0,255), 2)
 
-    cv2.imshow(windowName, frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#     cv2.imshow(windowName, frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-webcam.release()
-cv2.destroyAllWindows()
+# webcam.release()
+# cv2.destroyAllWindows()
 
 
 ################# IMG ##############
 
 
-# #choose an image file
-# img = cv2.imread('Profile-Russell.png')
-# grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#choose an image file
+img = cv2.imread('rock1.jpeg')
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-# print(face_coordinates) # list of coordinates (x, y, w, h)
-
-
-# for (x, y, w, h) in face_coordinates:
-#     cv2.rectangle(img, (x, y), (x+w, y+h), (0,0,255), 2)
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+print(face_coordinates) # list of coordinates (x, y, w, h)
 
 
-# cv2.imshow(windowName, img)
-# cv2.waitKey() # waits for a key stroke before exiting
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0,0,255), 2)
+
+
+cv2.imshow(windowName, img)
+cv2.waitKey() # waits for a key stroke before exiting
