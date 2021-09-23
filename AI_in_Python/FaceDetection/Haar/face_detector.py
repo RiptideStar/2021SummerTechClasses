a94013111d03fece18d863fb22c4917c34b779ed
Riptide_mac <kyle1001001@gmail.com>
2021-09-23 13:43:50 -0700

    thicker rectangle

--- AI_in_Python/FaceDetection/Haar/face_detector.py
+++ AI_in_Python/FaceDetection/Haar/face_detector.py
@@ -39,7 +39,7 @@ print(face_coordinates) # list of coordinates (x, y, w, h)
 
 
 for (x, y, w, h) in face_coordinates:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0,0,255), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0,0,255), 5)
 
 
 cv2.imshow(windowName, img)
